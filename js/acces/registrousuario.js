208ceb74fe08bcd536b8a2559cec65b13c24b617
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2021-03-20 14:50:29 -0500

    sistema de  acceso terminado

--- js/acces/registrousuario.js
+++ js/acces/registrousuario.js
@@ -1,30 +1,56 @@
 const crear = document.getElementById('registrar')
 crear.addEventListener('submit',registrar)
-function registrar() {
+async function registrar(e) {
+    e.preventDefault()
     //se reciben las variables de las cajas de texto
     let email = document.getElementById('Email').value;
     let contrasena = document.getElementById('password').value;
     let repicontrasena = document.getElementById('repitpassword').value;
    
-
-    firebase.auth().createUserWithEmailAndPassword(email, contrasena)
-        .then(function () {
+    if(contrasena === repicontrasena){
+    await firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+        .then( async function () {
             // si se registro correctamente en firebase
-            alert('Usuario registrado correctamente: ' + email)
-            verificarCorreo();
+           await Swal.fire({
+                title: 'Registro exitoso',
+                text: `Usuario registrado correctamente:  ${email}, se te enviara un correo de verificacion, y se te redireccionara 
+                al portal de inicio`,
+                icon: 'success',
+            })
+            .then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    window.location.href='/index.html'
+                    verificarCorreo()
+                }})
         })
         .catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             if (errorCode == 'auth/email-already-in-use') {
-                alert('El usuario ya existe, por favor inicia sesión \nSi olvidaste tu cotraseña puedes recuperarla');
+                Swal.fire({
+                    title: 'Error',
+                    text: `El usuario ya existe, por favor inicia sesión \nSi olvidaste tu cotraseña puedes recuperarla`,
+                    icon: 'Error',
+                })
             } else {
-                alert('Error de codigo:\n ' + errorCode + ' Mensaje de Error:\n ' + errorMessage);
+                Swal.fire({
+                    title: 'Error',
+                    text: `Error de codigo:\n ' + errorCode + ' Mensaje de Error:\n ' + errorMessage`,
+                    icon: 'Error',
+                })
+               
             }
 
         });
-
+    }else {
+        Swal.fire({
+            title: 'Error',
+            text: "Las contraseñas no coinciden, verifica e intenta nuevamente",
+            icon: 'Error',
+        })
+    }
 }
 // función que sirve enviar un correo a un usuario que se registre por primera vez
 function verificarCorreo() {
@@ -32,7 +58,6 @@ function verificarCorreo() {
     user.sendEmailVerification().then(function () {
         // Email sent.
         console.log('Se ha enviado un correo de notificación');
-        alert('Se ha enviado un correo de verificación de correo');
     }).catch(function (error) {
         // An error happened.
         console.log('Error, no se ha podido enviar un correo de notificación');
